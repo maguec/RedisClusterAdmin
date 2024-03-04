1c62ee458cfe0d3ea41402c0eaafa398d93ba292
Chris Mague <github@mague.com>
2024-03-04 11:09:21 -0800

    quick script

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/maguec/RedisClusterAdmin
+
+go 1.21.1
+
+require (
+	github.com/alexflint/go-arg v1.4.3
+	github.com/go-redis/redis/v9 v9.0.0-rc.2
+)
+
+require (
+	github.com/alexflint/go-scalar v1.1.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+)
---
+++ rcadmin.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/alexflint/go-arg"
+	"github.com/go-redis/redis/v9"
+	"os"
+	"strings"
+)
+
+var args struct {
+	ClusterServer string `help:"Cluster Server Host" default:"localhost" arg:"--server, -s, env:CLUSTER_SERVER"`
+	ClusterPort   string `help:"Cluster Server Port" default:"6379" arg:"--port, -p, env:CLUSTER_PORT"`
+	Verbose       bool   `help:"Verbose" arg:"--verbose, -v"`
+	Command       string `help:"Command" arg:"positional" required:"true"`
+}
+
+func getMasterNodes(conf *redis.ClusterOptions) ([]string, error) {
+	var ctx = context.Background()
+	nodes := []string{}
+	client := redis.NewClusterClient(conf)
+	slots, err := client.ClusterSlots(ctx).Result()
+	if err != nil {
+		return nil, err
+	}
+	for _, s := range slots {
+		nodes = append(nodes, s.Nodes[0].Addr)
+	}
+	return nodes, nil
+}
+
+func main() {
+	arg.MustParse(&args)
+	conf := &redis.ClusterOptions{
+		Addrs: []string{fmt.Sprintf("%s:%s", args.ClusterServer, args.ClusterPort)},
+	}
+	nodes, err := getMasterNodes(conf)
+	if err != nil {
+		panic(err)
+	}
+	for _, n := range nodes {
+		if args.Verbose {
+			fmt.Fprintf(os.Stderr, "# %s\n", n)
+		}
+		rdb := redis.NewClient(&redis.Options{Addr: n})
+		c := strings.Split(args.Command, " ")
+		cmd := make([]interface{}, len(c))
+		for i, v := range c {
+			cmd[i] = v
+		}
+		res, err := rdb.Do(context.Background(), cmd...).Result()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintln(os.Stdout, res)
+	}
+
+}
